c26d13474ad67fcf0017b1671d6736d692bd0d83
Abdullah Mohamed <3abdullah7mohamed@gmail.com>
2023-11-29 23:50:52 +0200

    last

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,109 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <v-stage :width="width" :height="height">
+      <v-layer>
+        <!-- Render Shapes -->
+        <v-rect
+          v-for="rect in shapes.rectangles"
+          :key="rect.id"
+          :x="rect.x"
+          :y="rect.y"
+          :width="rect.width"
+          :height="rect.height"
+          :fill="rect.fill"
+          :draggable="true"
+          @click="handleShapeClick(rect)"
+        />
+
+        <v-circle
+          v-for="circle in shapes.circles"
+          :key="circle.id"
+          :x="circle.x"
+          :y="circle.y"
+          :radius="circle.radius"
+          :fill="circle.fill"
+          @click="handleShapeClick(circle)"
+        />
+
+        <v-line
+          v-for="line in shapes.lines"
+          :key="line.id"
+          :points="line.points"
+          :stroke="line.stroke"
+          :strokeWidth="line.strokeWidth"
+          @click="handleShapeClick(line)"
+          
+        />
+      </v-layer>
+    </v-stage>
+
+    
+    <div>
+      <button @click="addRectangle">Add Rectangle</button>
+      <button @click="addCircle">Add Circle</button>
+      <button @click="addLine">Add Line</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
+  },
+  data() {
+    return {
+      width: 800,
+      height: 600,
+      shapes: {
+        rectangles: [],
+        circles: [],
+        lines: [],
+      },
+      shapeIdCounter: 1,
+    };
+  },
+  methods: {
+    handleShapeClick(shape) {
+      console.log(shape.x);
+
+    },
+    addRectangle() {
+
+      this.shapes.rectangles.push({
+        id: this.shapeIdCounter++,
+        x: 50,
+        y: 50,
+        width: 100,
+        height: 80,
+        fill: 'red',
+        DragEvent:true
+      });
+      
+    },
+    printX(){
+      
+    },
+    addCircle() {
+      this.shapes.circles.push({
+        id: this.shapeIdCounter++,
+        x: 200,
+        y: 150,
+        radius: 50,
+        fill: 'blue',
+      });
+    },
+    addLine() {
+      this.shapes.lines.push({
+        id: this.shapeIdCounter++,
+        points: [300, 300, 400, 400],
+        stroke: 'green',
+        strokeWidth: 5,
+      });
+    },
   }
 }
 </script>
